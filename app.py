e757495513748db8f829227783dbec7b926433e4
AlexanderRafael <75244564@continental.edu.pe>
2025-10-26 21:07:43 -0500

    flask

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask
 import os
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,5 +7,5 @@ def home():
     return "<h1>¡Hola MUNDO DESDE Render! </h1><p>La aplicación en python está funcionando correctamente.</p>"
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
